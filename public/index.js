875f89c606988973058003b367d78797b0460e6d
Darill <darilinsankamil2132@gmail.com>
2023-11-09 09:17:59 +0700

    add mobile navbar

--- public/index.js
+++ public/index.js
@@ -1,37 +1,8 @@
-const gallery = document.getElementById('gallery-section');
-const data = [
-    {
-        id:1,
-        alt:"rafting-1",
-        img: '/assets/image/rafting-1.png'
-    },
-    {
-        id:2,
-        alt:"rafting-1",
-        img: '/assets/image/rafting-1.png'
-    },
-    {
-        id:3,
-        alt:"rafting-1",
-        img: '/assets/image/rafting-1.png'
-    },
-    {
-        id:4,
-        alt:"rafting-1",
-        img: '/assets/image/rafting-2.png'
-    },
-    {
-        id:5,
-        alt:"rafting-1",
-        img: '/assets/image/rafting-2.png'
-    },
-    {
-        id:6,
-        alt:"rafting-1",
-        img: '/assets/image/rafting-2.png'
-    },
-]
-// for (let i = 0; i < data.length; i++) {
-//     gallery.innerHTML = `<img src=${data[i].img} alt="rafting-1"/>`
-//     console.log(data[i].img)
-// }
+function toggleSidebar() {
+    let sidebar = document.getElementById("mobile");
+    if (sidebar.classList.contains("hidden")) {
+        sidebar.classList.remove("hidden");
+    } else {
+        sidebar.classList.add("hidden");
+    }
+}
